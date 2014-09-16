9f4fcc17c256e4480c8f805645a3e07a99af45c3
Liu Xuefeng <liuxf@jaist.ac.jp>
2014-09-16 17:07:31 +0900

    Finish updating route.rb and generate User with new action

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,48 +1,30 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+  subject { page }
   describe "Home Page" do
-    it "should have the content 'Sample App'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-    it "should have the right title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Home")
-    end
+    before { visit root_path }
+    
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should have_title('| Home') }
   end
 
   describe "Help page" do
-  	it "should have the content 'Help'" do
-  	  visit '/static_pages/help'
-  	  expect(page).to have_content('Help')
-  	end
-  	it "should have the right title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Help")
-    end
+    before { visit help_path }
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
-  	it "should have the content 'About Us'" do
-  	  visit '/static_pages/about'
-  	  expect(page).to have_content('About Us')
-  	end
-  	it "should have the right title 'About'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("About")
-    end
+    before { visit about_path }
+    it { should have_content('About') }
+    it { should have_title(full_title('About')) }
+  end
 
-    describe "Contact page" do
-      it "should have the content 'Contact us'" do
-      	visit '/static_pages/contact'
-      	expect(page).to have_content('Contact us')
-  	  end
-  	  it "should have the right title 'Contact'" do
-  	  	visit '/static_pages/contact'
-  	  	expect(page).to have_title("Contact")
-  	  end
-  	end
+  describe "Contact page" do
+    before { visit contact_path }
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
