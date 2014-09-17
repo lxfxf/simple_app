8eae43522f365a937f82381d6b70a9cc6e89772e
Liu Xuefeng <liuxf@jaist.ac.jp>
2014-09-17 15:46:39 +0900

    Make a basic User model(including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
 	validates :email, presence: true,
 									  format: { with: VALID_EMAIL_REGEX },
 						 				uniqueness: { case_sensitive: false }
+	has_secure_password
+	validates :password, length: { minimum: 6 }
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,22 @@
 require 'spec_helper'
 
 describe User do
-	before { @user = User.new(name: "Liu Xuefeng", email: "liuxuefeng@icloud.com") }
+	before do
+	 	@user = User.new(name: "Liu Xuefeng", 
+ 										 email: "liuxuefeng@icloud.com",
+ 										 password: "foobar",
+ 										 password_confirmation: "foobar") 
+	end
 	subject{ @user }
 
 	it { should respond_to(:name) }
 	it { should respond_to(:email) }
 	it { should respond_to(:password_digest) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
 	it { should be_valid }
 
-	describe "when  name is not present" do
+	describe "when name is not present" do
 		before { @user.name = " " }
 		it { should_not be_valid }
 	end
@@ -53,5 +60,49 @@ describe User do
 		end
 		it { should_not be_valid }
 	end
+
+	describe "when password is not present" do
+		before do
+			@user = User.new(name: "Liu Xuefeng", email: "liuxuefeng@me.com",
+											 password: " ", password_confirmation: " ")
+		end
+		it { should_not be_valid }
+	end
+
+	describe "when passowrd doesn't match confirmation" do
+		before { @user.password_confirmation = "mismatch" }
+		it { should_not be_valid }
+	end
+
+	describe "return value of authenticate method" do
+		before { @user.save }
+		let(:found_user) { User.find_by(email: @user.email) }
+
+		describe "with valid password" do
+			it { should eq found_user.authenticate(@user.password) }
+		end
+		describe "with invalid password" do
+			let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+			it { should_not eq user_for_invalid_password }
+			specify { expect(user_for_invalid_password).to be_false }
+		end
+	end
+
+	describe "with a password that's too short" do
+		before { @user.password = @user.password_confirmation = "a" * 5 }
+		it { should_not be_valid }
+	end
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
