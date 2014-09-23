4d36b2b5d5896f89ef98f487b441564fc97210e0
Liu Xuefeng <liuxf@jaist.ac.jp>
2014-09-23 13:51:07 +0900

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,6 +9,32 @@ describe "Static Pages" do
     it { should have_content('Sample App') }
     it { should have_title(full_title('')) }
     it { should have_title('| Home') }
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          expect(page).to have_selector("li##{item.id}", text: item.content)
+        end
+      end
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
+
+    end
+
   end
 
   describe "Help page" do
@@ -39,18 +65,5 @@ describe "Static Pages" do
     end
   end
 
-  describe "for signed-in users" do
-    let(:user) { FactoryGirl.create(:user) }
-    before do
-      FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-      FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
-      sign_in user
-      visit root_path
-    end
-    it "should render the user's feed" do
-      user.feed.each do |item|
-        expect(page).to have_selector("li##{item.id}", text: item.content)
-      end
-    end
-  end
+
 end
